afa107d87cb2f2c82ad76171c1fd75395ad700c6
Rostislav <77685422+rostislav-frontend@users.noreply.github.com>
2022-11-03 23:49:12 +0300

    fix app buttons

--- telegram-bot/src/app.buttons.ts
+++ telegram-bot/src/app.buttons.ts
@@ -1,11 +1,19 @@
 import { Markup } from 'telegraf';
 
+
+export const listBtn = 'Список дел'
+export const doneBtn = '✅ Завершить';
+export const editBtn = 'Редактирование';
+export const deleteBtn = '❌ Удаление';
+
+
 export const actionButtons = () => {
     return Markup.keyboard(
         [
-            Markup.button.callback('Авторизация VK', 'auth'),
-            Markup.button.callback('Информация о боте', 'info'),
-            Markup.button.callback('Мимо проходил', 'exit'),
+            Markup.button.callback(listBtn, 'list'),
+            Markup.button.callback(doneBtn, 'done'),
+            Markup.button.callback(editBtn, 'edit'),
+            Markup.button.callback(deleteBtn, 'delete'),
         ],
         {
             columns: 1
